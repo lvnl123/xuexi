c3e6a4435f3473e592ffe7a9f19cc206f4e5e25c
lvnl123 <122267601+lvnl123@users.noreply.github.com>
2024-10-13 10:06:35 +0800

    Add files via upload

---
+++ 解包.py
@@ -0,0 +1,36 @@
+import os
+
+total_bundles = 0
+success_count = 0
+error_count = 0
+
+
+def decrypt_and_save(file_path):
+    global total_bundles, success_count, error_count
+    total_bundles += 1
+    print(file_path)
+    with open(file_path, "rb") as f:
+        data = f.read()
+    result = bytearray(data)[50:]
+    key = result[0] ^ 0x55  # U
+    if key != result[1] ^ 0x6E:  # n
+        print("Decryption Error: Not an Unity File!")
+        error_count += 1
+    else:
+        result = [byte ^ key for byte in result]
+        decrypted_file_path = file_path + ".decr"
+        with open(decrypted_file_path, "wb") as f:
+            f.write(bytes(result))
+        success_count += 1
+
+
+if __name__ == "__main__":
+    custom_dir = input("请输入要解密文件所在的目录路径：")
+
+    for root, dirs, files in os.walk(custom_dir):
+        for file in files:
+            if file.endswith(".bundle"):
+                decrypt_and_save(os.path.join(root, file))
+
+    print(f"\nDecryption completed. {total_bundles} files in total, {success_count} successful, {error_count} failed.")
+    input("Press Enter to exit ...")
