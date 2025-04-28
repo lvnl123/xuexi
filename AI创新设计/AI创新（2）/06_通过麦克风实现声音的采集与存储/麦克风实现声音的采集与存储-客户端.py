d1d3044d0490509150ec5b9e17b7dfbc0a991b68
lvnl123 <122267601+lvnl123@users.noreply.github.com>
2025-04-28 17:03:29 +0800

    Delete AI创新设计/AI创新（2）/06_通过麦克风实现声音的采集与存储/麦克风实现声音的采集与存储-客户端.py

--- AI创新设计/AI创新（2）/06_通过麦克风实现声音的采集与存储/麦克风实现声音的采集与存储-客户端.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-# 测试 FFmpeg 是否可用
-try:
-    result = subprocess.run(["ffmpeg", "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print("FFmpeg 已找到！版本信息如下：")
-    print(result.stdout)
-except FileNotFoundError:
-    print("未找到 FFmpeg，请确保已安装并添加到系统路径中。")
